5281767dfd481e3848bab4e9c7e90acdea30db5f
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-12-02 00:06:09 -0300

    Criar entidade User

---
+++ booklore/booklore/src/test/java/com/jhcs/booklore/BookloreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhcs.booklore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookloreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
